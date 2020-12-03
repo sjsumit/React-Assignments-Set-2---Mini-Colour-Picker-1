bb61240d11927cb80a273e125e07fc111df68ea5
Janani998 <68743120+Janani998@users.noreply.github.com>
2020-12-03 23:09:06 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ const title = "Select the gradient and then the Box to change the color";
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ background: "" });
   const applyColor = (updateSelectionStyle) => {
-    updateSelectionStyle(nextBackground.background);
+    updateSelectionStyle(nextBackground);
   };
   return (
     <div id="master">
@@ -47,11 +47,7 @@ const App = () => {
 
       <div className="row" id="children-wrapper">
         {["selection1", "selection2", "selection3"].map((key) => (
-          <Selection
-            key={key}
-            applyColor={applyColor}
-            background={nextBackground.background}
-          />
+          <Selection key={key} applyColor={applyColor} />
         ))}
       </div>
     </div>
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "../styles/Child.css";
 
-export default function Selection({ applyColor, background }) {
+export default function Selection({ applyColor }) {
   const [selectedBackground, setSelectedBackground] = React.useState({
-    background
+    background: ""
   });
   return (
     <div
       className="fix-box"
       onClick={() => applyColor(setSelectedBackground)}
-      style={{ background: selectedBackground }}
+      style={{ background: selectedBackground.background }}
     >
       <h2 className="subheading">Selection</h2>
     </div>
